2402471d48fc5eadd5ed6a8840cc339fd348c3ff
Titas <titas@pixylt.com>
2022-04-28 10:29:59 +0200

    feat: :heavy_plus_sign: Installed TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '599px',
+      // => @media (min-width: 599px) { ... }
+
+      'md': '1023px',
+      // => @media (min-width: 1023px) { ... }
+
+      'lg': '1920px',
+      // => @media (min-width: 1920px) { ... }
+    },
+    spacing: {
+      sm: '16px',
+      md: '24px',
+      lg: '24px',
+    }
+  },
+  plugins: [],
+}

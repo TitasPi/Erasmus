7e3f955e97e53af241a3dfa1e6e694d5a668d0b6
Titas <titas@pixylt.com>
2022-05-19 08:47:36 +0200

    feat: :sparkles: Updated Album and Collection models

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Album extends Model
 {
-    use HasFactory;
+    use HasFactory, HasTranslations;
+
+    public $translatable = ['title', 'short_description', 'long_description'];
+
+    protected $fillable = [
+        'title',
+        'short_description',
+        'long_description',
+        'date',
+        'collection_id'
+    ];
 
     public function images() {
         return $this->hasMany(Image::class);
--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Collection extends Model
 {
-    use HasFactory;
+    use HasFactory, HasTranslations;
+
+    public $translatable = ['name', 'short_description'];
 
     protected $fillable = [
         'name',

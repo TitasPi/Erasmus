bb91c40092b4de8e18f1f9b065c1693e5a904786
Titas <titas@pixylt.com>
2022-05-19 08:48:39 +0200

    feat: :lipstick: Footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,15 @@
+<div class="pt-10">
+  <div class="flex justify-evenly px-96 py-5 text-4xl">
+    @foreach(\App\Models\Collection::where('active', True)->get() as $collection)
+      <a href="#linkToCollection-{{ $collection->id }}" class="{{ $collection->icon }} hover:text-[#3C3C3C] text-[#7B7B7B]"></a>
+    @endforeach
+  </div>
+  <div class="flex justify-evenly px-96 py-5 text-4xl">
+    <a href="">{{ __('footer.about_me') }}</a>
+    <a href="">{{ __('footer.privacy_policy') }}</a>
+    <a href="">{{ __('footer.contact') }}</a>
+  </div>
+  <div class="flex justify-center px-96 py-5">
+    <a href=""><img src="{{ asset('svg/instagram.svg') }}" alt=""></a>
+  </div>
+</div>

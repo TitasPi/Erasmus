20e40e5dbaf40bc67379a4bfe0115570c6463ee9
Titas <titas@pixylt.com>
2022-05-23 08:38:54 +0200

    feat: :sparkles: Translatable strings in about & contact pages. Close #7

--- resources/views/about/form.blade.php
+++ resources/views/about/form.blade.php
@@ -1,8 +1,8 @@
 <div class='w-1/2 ml-auto pr-28'>
   <div class='font-semibold pt-28'>
-    <p class='text-4xl'>Let's get in touch!</p>
-    <p class='text-2xl'>Question? Suggestion? Project?</p>
-    <p class='text-2xl'>Don't hesitate to contact me</p>
+    <p class='text-4xl'>{{ __('contact.firstLine') }}</p>
+    <p class='text-2xl'>{{ __('contact.secondLine') }}</p>
+    <p class='text-2xl'>{{ __('contact.thirdLine') }}</p>
   </div>
   <div class='mt-10'>
     <form action="" method="post">
@@ -22,18 +22,18 @@
       @endif
       @csrf
       <div class="mb-3 xl:w-96">
-        <label for="name" class="form-label inline-block mb-2 text-gray-700">Your name</label>
-        <input type="text" class="form-input" id="name" name="name" placeholder="How should we call you?" value="{{ old('name') }}" />
+        <label for="name" class="form-label inline-block mb-2 text-gray-700">{{ __('contact.yourName') }}</label>
+        <input type="text" class="form-input" id="name" name="name" placeholder="{{ __('contact.yourName.help') }}" value="{{ old('name') }}" />
       </div>
       <div class="mb-3 xl:w-96">
-        <label for="email" class="form-label inline-block mb-2 text-gray-700">Your email</label>
-        <input type="email" class="form-input" id="email" name="email" placeholder="How should we contact you?" value="{{ old('email') }}" />
+        <label for="email" class="form-label inline-block mb-2 text-gray-700">{{ __('contact.yourEmail') }}</label>
+        <input type="email" class="form-input" id="email" name="email" placeholder="{{ __('contact.yourEmail.help') }}" value="{{ old('email') }}" />
       </div>
       <div class="mb-3 xl:w-96">
-        <label for="message" class="form-label inline-block mb-2 text-gray-700">Your message</label>
-        <textarea class="form-input" id="message" name="message" rows="3" placeholder="What would you like to talk about?">{{ old('message') }}</textarea>
+        <label for="message" class="form-label inline-block mb-2 text-gray-700">{{ __('contact.yourMessage') }}</label>
+        <textarea class="form-input" id="message" name="message" rows="3" placeholder="{{ __('contact.yourMessage.help') }}">{{ old('message') }}</textarea>
       </div>
-      <button type="submit" class='btn-success'>Send</button>
+      <button type="submit" class='btn-success'>{{ __('contact.send') }} Send</button>
     </form>
   </div>
 </div>
--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -12,7 +12,7 @@
       @include('welcome-nav')
 
       <div class="custom-font -rotate-90 text-9xl relative left-[-40%] top-60">
-        <p>My story</p>
+        <p>{{ __('about.title') }}</p>
       </div>
     </div>
   </div>
@@ -22,7 +22,7 @@
 @section('content')
   <div>
     <p class='w-1/2 m-auto pt-28 text-2xl'>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt at esse veniam voluptatem quis dolorem rerum illum aperiam debitis dolore, commodi nisi accusantium unde repudiandae consectetur eius reprehenderit mollitia impedit natus ab suscipit dicta laborum. Enim beatae incidunt, fuga dolorum voluptate nam molestias maxime accusamus quam suscipit voluptatem excepturi porro, accusantium optio voluptas nihil explicabo ullam deserunt. Eveniet at harum earum voluptates dolor explicabo accusamus deserunt eaque assumenda nemo est, doloremque temporibus id modi excepturi saepe fugit odio aperiam distinctio rem nisi! Error sed nobis, rerum temporibus officia quia minima. Necessitatibus natus reiciendis corrupti reprehenderit enim odio amet ipsam ullam.
+      {{ __('about.description') }}
     </p>
   </div>
 

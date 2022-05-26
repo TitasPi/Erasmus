e28f2a9cb741f92499819d522d41c82d118f2266
Titas <titas@pixylt.com>
2022-05-26 10:22:58 +0200

    feat: :sparkles: Collections preview. Close #10

--- resources/views/collections/list.blade.php
+++ resources/views/collections/list.blade.php
@@ -10,10 +10,14 @@
     <img src="{{ asset('img/welcome-background.jpg') }}" alt="" class="h-screen w-[50vw] object-cover img-load opacity-0">
     <div>
       @include('welcome-nav')
-      <div class="rotate-90 fixed left-10 top-80">
-        <div>
+      <div>
+        <div class='rotate-90 fixed left-10 top-80'>
           <p class="text-9xl">{{$collection->name}}</p>
           <p class="custom-font text-8xl">Yoann Baty</p>
+        </div>
+        <div class='m-20'>
+          <p class='text-5xl my-5'>{{ $collection->name }}</p>
+          <p class='text-3xl'>{{ $collection->short_description }}</p>
         </div>
       </div>
     </div>
@@ -30,10 +34,14 @@
       $album = $albums[$x];
     @endphp
     <div class='columns-2'>
-      <div></div>
-      <div></div>
+      <div class='col-span-1 w-[50vw]'>
+        <img src="/images/{{$album->images[0]->src}}" alt="" class="w-1/2 mx-auto opacity-0">
+      </div>
+      <div class='text-center group' onclick='window.location.href="{{ route("album", ["lang" => App::currentLocale(), "collection" => $collection->id, "album" => $album->id]) }}"'>
+        <p class='relative rotate-90 text-7xl top-40 -right-80 group-hover:text-8xl duration-300'>{{ $album->title }}</p>
+        <img src="/images/{{$album->images[0]->src}}" alt="" class="w-1/2 mx-auto">
+      </div>
     </div>
-    {{$album}}
   @endfor
 @endsection
 

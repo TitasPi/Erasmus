47c30158207d20390a1cdd228b220ef4d3e585f5
Titas <titasp@pixylt.com>
2022-06-01 22:15:11 +0200

    chore: :bookmark: Bumped up the version

--- config/self-update.php
+++ config/self-update.php
@@ -24,7 +24,7 @@ return [
     |
     */
 
-    'version_installed' => env('SELF_UPDATER_VERSION_INSTALLED', '0.1.3'),
+    'version_installed' => env('SELF_UPDATER_VERSION_INSTALLED', '0.1.5'),
 
     /*
     |--------------------------------------------------------------------------
